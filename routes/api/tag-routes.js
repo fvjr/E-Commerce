3d41d39851f818bec6478d02e19bb187eeec7500
Fernando Vasquez <vasquezjrf@gmail.com>
2022-03-02 18:57:31 -0500

    establized sequelize connection, updated tag route functionality

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { restore } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll();
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
@@ -13,9 +20,18 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
-});
+  //tag_name
+  try {
+    const tagData = await Tag.create(req.body);
+    console.log(req.body)
+    res.status(200).json(tagData)
+    } catch (err){
+      res.status(400).json(err)
+    }
+  }
+);
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
