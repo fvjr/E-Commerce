3959d2d8f1e0803914dafb7c9e5a3637b7844343
Fernando Vasquez <vasquezjrf@gmail.com>
2022-03-02 19:48:37 -0500

    added update route to tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -45,26 +45,44 @@ router.post('/', async (req, res) => {
 }
 );
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      },
+    },
+    )
+    res.status(200).json(tagData)
+} catch(err){
+  res.status(500).json(err)
+}
+
+
+
 });
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
-  try{
+  try {
     const tagData = await Tag.destroy({
       where: {
         id: req.params.id
       }
     })
 
-if (!tagData) {
-  res.status(404).json({message: 'No tag found with this ID'});
-  return
-}
-res.status(200).json(tagData)
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this ID' });
+      return
+    }
+    res.status(200).json(tagData)
 
-  } catch(err){
+  } catch (err) {
     res.status(500).json(err);
   }
 });
