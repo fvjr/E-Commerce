e455f99da26533929274ec30f4f0572f0ad19bd7
Fernando Vasquez <vasquezjrf@gmail.com>
2022-03-02 20:36:17 -0500

    added update route to category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,16 +38,32 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
-try {
-  const categoryData = await Category.create(req.body);
-  res.status(200).json(categoryData);
-} catch (err){
-  res.status(400).json(err)
-}
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        },
+      },
+    )
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+
 });
 
 router.delete('/:id', (req, res) => {
